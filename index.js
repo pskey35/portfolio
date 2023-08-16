1ff3b5efd9982cf3d43ee406bc4ef11f594542aa
pskey35 <anonimo35371@gmail.com>
2023-08-16 14:22:45 -0500

    agregado carpeta public

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 const app = require("./src/app")
 const port = process.env.PORT || 8000
+console.log(port)
 app.listen(port)
--- src/app.js
+++ src/app.js
@@ -1,9 +1,59 @@
-const express = require("express")
-const app = express()
-
+const express = require("express");
+const app = express();
+//const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const nodeMailer = require("nodemailer")
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(cors())
+/*aqui use el cors para que se pueda recibir cookies
+en el fetch agregar esto credentials: "include",
+y en el servidor poner esto de aqui abajo si no.no funciona
+*/
+//el cors no funciona en vercel
+/*
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true   
+  }));*/
+/* 
 app.get("/",(req,res)=>{
     const ip =req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.send(`esta es tu ip: ${ip}`)
 })
+*/
+app.use(express.static(path.join(__dirname, "/../public")));
+app.post("/data", (req, res) => {
+  //const a = req.body;
+  //res.cookie("mensajes","saludo")
+  console.log(req.body)
+  console.log(req.cookies)
+  const transport = nodeMailer.createTransport({
+    host:"smtp.gmail.com",
+    port:465,
+    secure:true,
+    auth:{
+        user:"jayme35371@gmail.com",
+        pass:"jaime1020.desk"
+    }
+  })
+  const mensaje = {
+    from:"jayme35371gmail.com",
+    to:"anonimo35371@gmail.com",
+    subject:"portafolio contacto",
+    text:`input1:${req.body.input1},,,, input2:${req.body.input2}`
+  }
 
-module.exports = app
+  transport.sendMail(mensaje,(err,info)=>{
+    console.log(info)
+    console.log(err)
+  })
+  res.json({estado:"creado cookie correctamente"})
+
+  //se tiene que enviar algo al cliente para que pueda 
+  //crear correctamente la cookie puede ser un res.send() o res.json()
+});
+
+
+module.exports = app;
